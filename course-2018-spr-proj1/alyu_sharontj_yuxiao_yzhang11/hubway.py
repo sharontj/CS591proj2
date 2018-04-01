0d001f90b7b5cbdd42784c2f3d274b2e3586cbab
o1xhack <o1xhack@gmail.com>
2018-04-01 16:29:06 -0400

    Hubway add exist database, remove not exist station

--- course-2018-spr-proj1/alyu_sharontj_yuxiao_yzhang11/hubway.py
+++ course-2018-spr-proj1/alyu_sharontj_yuxiao_yzhang11/hubway.py
@@ -32,7 +32,7 @@ def csvConvert():
 class hubway(dml.Algorithm):
     contributor = 'alyu_sharontj_yuxiao_yzhang11'
     reads = []
-    writes = ['alyu_sharontj_yuxiao_yzhang11.hubway']
+    writes = ['alyu_sharontj_yuxiao_yzhang11.hubway','alyu_sharontj_yuxiao_yzhang11.hubway_exist']
 
     @staticmethod
     def execute(trial=True):
@@ -51,6 +51,21 @@ class hubway(dml.Algorithm):
         repo.dropCollection("hubway")
         repo.createCollection("hubway")
         repo['alyu_sharontj_yuxiao_yzhang11.hubway'].insert_many(dict_values)
+
+        repo.dropCollection("hubway_exist")
+        repo.createCollection("hubway_exist")
+        hubway = repo['alyu_sharontj_yuxiao_yzhang11.hubway']
+        match = {
+            'status': "Existing"
+        }
+
+        hubwayExist = hubway.aggregate([
+            {
+                '$match': match
+            }
+        ])
+
+        repo['alyu_sharontj_yuxiao_yzhang11.hubway_exist'].insert(hubwayExist)
 
         endTime = datetime.datetime.now()
 
